03bf270b061f2d6543f850f2d62736d79f744a59
Aden Dean <Aden.Dean@us.kbr.com>
2024-07-11 15:40:36 -0400

    Accounts for distances up to 8 meters

--- Radar_Combined.ino
+++ Radar_Combined.ino
@@ -6,7 +6,8 @@ String pypos;
 int trigPin = 12;  // Trigger
 int echoPin = 11;  // Echo
 int pos = 0;
-long duration, cm, inches;
+long duration;
+float cm;
 char userInput;
 
 void setup() {
@@ -37,18 +38,43 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
 
   // Convert the time into a distance
-  cm = (duration / 2) / 29.1;  // Divide by 29.1 or multiply by 0.0343
+  cm = (duration / 2) * 0.0343;  // Divide by 29.1 or multiply by 0.0343
 
 
   Serial.print(cm);
   Serial.println();
 
-  delay(5);
+  if (cm <= 10){
+    delay(2);
+  } else if (cm <= 50) {
+    delay(4);
+  } else if (cm <= 100) {
+    delay(7);
+  } else if (cm <= 150) {
+    delay(9);
+  } else if (cm <= 200) {
+    delay(12);
+  } else if (cm <= 250) {
+    delay(15);
+  } else if (cm <= 300) {
+    delay(18);
+  } else if (cm <= 350) {
+    delay(21);
+  } else if (cm <= 400) {
+    delay(24);
+  } else if (cm <= 450) {
+    delay(27);
+  } else {
+    delay(47);
+  }
 
   if (Serial.available()) {
     pypos = Serial.readStringUntil('\n');
     int pyposint = pypos.toInt();
     myservo.write(pyposint);
+    delay(2);
+  }
+  else {
     delay(2);
   }
 }
