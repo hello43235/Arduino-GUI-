99a87733c54a34d1772acb1e19ea89a7680dc096
Aden Dean <Aden.Dean@us.kbr.com>
2024-07-11 12:42:48 -0400

    Added Threshold Upper and Lower Bounds

--- CustomDialog.py
+++ CustomDialog.py
@@ -13,16 +13,48 @@ class CustomDialog(QDialog):
 
         self.setWindowTitle("Enter Arduino Settings")
 
-        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        #Layout
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
 
-        self.buttonBox = QDialogButtonBox(QBtn)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
+        # Port ComboBox and Labels
+        com_list = []
+        port_1 = QLabel()
+        # Find available ports to connect to
+        ports = serial.tools.list_ports.comports()
 
-        self.port = QLineEdit("COM4")
-        self.threshold_number = QLineEdit("20")
+        self.port = QComboBox()
+        self.port.setMaxCount(len(ports))
+
+        for index, value in enumerate(sorted(ports)):
+            print(index, '\t', value.name, '\t', value.description)
+            com_list.append(value.name + '\t' + value.description)
+            self.port.addItem(value.name)
+        print(len(ports), 'ports found')
+
+        message = QLabel("Select Port Name")
+        port_1.setText("Available Ports:\n" + '\n'.join(com_list))
+
+        self.layout.addWidget(port_1, 0, 0)
+        self.layout.addWidget(message, 1, 0)
+        self.layout.addWidget(self.port, 2, 0)
+
+        # Threshold Detection Bounds Labels and LineEdits
+        self.threshold_bound1 = QLineEdit("20")
+        self.threshold_bound2 = QLineEdit("0")
+
+        self.layout.addWidget(QLabel("Enter Threshold Detection Bound 1 (cm)"), 3, 0)
+        self.layout.addWidget(self.threshold_bound1, 4, 0)
+        self.layout.addWidget(QLabel("Enter Threshold Detection Bound 2 (cm)"), 3, 1)
+        self.layout.addWidget(self.threshold_bound2, 4, 1)
+
+        # Maximum Range Label and LineEdit
         self.limit = QLineEdit("50")
 
+        self.layout.addWidget(QLabel("Enter the Maximum distance to detect (cm)"), 1, 1)
+        self.layout.addWidget(self.limit, 2, 1)
+
+        # Detection Radius Slider
         self.detection_radius = QSlider(Qt.Orientation.Horizontal)
         self.detection_radius.setRange(3, 18)
         self.detection_radius.setSingleStep(1)
@@ -31,31 +63,17 @@ class CustomDialog(QDialog):
         self.detection_radius.setTickPosition(QSlider.TicksAbove)
         self.detection_label = QLabel("Enter The Scanning Radius (degrees): 180")
         self.detection_radius.valueChanged.connect(self.update_value)
-        self.layout = QGridLayout()
 
-        com_list = []
-        port_1 = QLabel()
-        # Find available ports to connect to
-        ports = serial.tools.list_ports.comports()
-        for index, value in enumerate(sorted(ports)):
-            print(index, '\t', value.name, '\t', value.description)
-            com_list.append(value.name + '\t' + value.description)
-        print(len(ports), 'ports found')
-
-        message = QLabel("Enter Port Name")
-        port_1.setText("Available Ports:\n" + '\n'.join(com_list))
-
-        self.layout.addWidget(port_1, 0, 0)
-        self.layout.addWidget(message, 1, 0)
-        self.layout.addWidget(self.port, 2, 0)
-        self.layout.addWidget(QLabel("Enter Threshold Detection Range (cm)"), 1, 1)
-        self.layout.addWidget(self.threshold_number, 2, 1)
-        self.layout.addWidget(QLabel("Enter the Maximum distance to detect (cm)"), 3, 0)
-        self.layout.addWidget(self.limit, 4, 0, 1, 2)
         self.layout.addWidget(self.detection_label, 5, 0)
         self.layout.addWidget(self.detection_radius, 6, 0, 1, 2)
+
+        # Button Box
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
         self.layout.addWidget(self.buttonBox, 7, 0, 1, 2, QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.setLayout(self.layout)
 
     def update_value(self):
         self.detection_label.setText("Enter The Scanning Radius (degrees): " + str(self.detection_radius.value() * 10))
